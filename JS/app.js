f9a67bbad3768cd91cc3b7196f221d24e47ea316
abdelqader-alomari <aboud.coding@gmail.com>
2021-05-23 18:53:13 +0300

    adding html,css,js files

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+alert('Hello, welcome to be here in the Coding forum, to register answer the following quesions')
+let fullName = prompt("What's your full name?");
+let userAge = prompt("How old are you?");
+let codingRate = prompt("How do you rate your coding skills out of 10?");
+let userEmail = prompt("What's your Email?");
+alert('Your Name is ' + fullName + ' , your age is ' + userAge + ' , your Coding rate is ' + codingRate + ' and your Email is ' + userEmail)
+alert("Hello my name is Abdelqader Al-Omari, will guide you through this nice Coding journey, nice to be with us.")
